90b5fee004d29520e1a55e2e9d169704f9d76b39
Franco Allemandi <franco_allemandi@hotmail.com>
2025-02-25 17:24:57 -0300

    de nuevo

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 //prueba
+//declaracion
+let var_prueba;
+//asignacion
+var_prueba='tito';
+
+console.log(var_prueba);
+
+//inicializar (declarar+asignar)
+let edad=37;
+
+console.log(edad);
+
+//const
+
+const nacimiento = 1987;
+console.log(nacimiento);
+
+const titulo=document.getElementById("titulo");
+titulo.textContent = "texto modificadinho";
